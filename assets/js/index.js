a264357538662852dd895f38fc033d69ee5d69a9
Chien81 <cheyennehunt78@gmail.com>
2020-08-10 21:06:22 -0700

    inital commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,12 @@ new Glide('.glide', {
     perView: 3
   }).mount()
 
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
